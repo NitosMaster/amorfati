9c8b0b14a60da971b3d6ce2a26ccdd2fcb7c8d61
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 <nunoarrochella@gmail.com>
2026-01-20 22:39:56 +0000

    Incomplete, going to continue tommorow if I can

--- src/amorfati/core/Window.cpp
+++ src/amorfati/core/Window.cpp
@@ -1,7 +1,31 @@
 #include "Window.h"
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<iostream>
 
 namespace amorfati {
-    void PrintEngineStatus () {
-        std::cout << "workin!\n";
+    Window::Window(int width, int height, const std::string& title) {
+        //«== if glfw fails to initialize ==»//
+        if (!glfwInit()) {
+            std::cerr << "GLFW was caught lackin!\n";
+            return;
+        }
+        
+        //«== using c_str() because glfw is a .c lib, idk if .cpp str may cause issues later==»//
+        //«==fullscreen with glfwGetPrimaryMonitor() ==»//
+        mWindow = glfwCreateWindow(width, height, title.c_str(), nullptr, glfwGetPrimaryMonitor());
+        
+        if (!mWindow) {
+            glfwTerminate();
+            return;
+        }
+
+        glfwMakeContextCurrent(mWindow);
+
+        //«== if glad fails to initialize ==»//
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+            std::cerr << "GLAD was caught lackin!\n";
+            return;
+        }
     }
 }
