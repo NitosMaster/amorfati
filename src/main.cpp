970dfe23b1dd480236f0df019994a58c7d7196c1
Nuno Lobo <141408823+NitosMaster@users.noreply.github.com>
2026-01-26 22:50:44 +0000

    kinda structured now
    i was in a rush so the last commit was just putting things for me to fix now

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,18 @@
 #include <amorfati/core/Shader.h>
 
 int main () {
-    amorfati::PrintEngineStatus();
-    
-    amorfati::Window window(1280, 720, "aim r fatty");
-
-    while (!window.ShouldClose()) {
-        window.Update(0, 0, 0, 1);
-    }
-    
     const char* vertexPath = "~/CompSci/amorfati/assets/shaders/test.vert";
     const char* fragPath = "~/CompSci/amorfati/assets/shaders/test.frag";
 
-    amorfati::Shader(vertexPath, fragPath);
-    useShader();
+    amorfati::printStatus()
+    amorfati::Window window(1280, 720, "aim r fatty");
+    amorfati::Shader test(vertexPath, fragPath);
+
+    while (!window.ShouldClose()) {
+        window.Update(0, 0, 0, 1);
+        test.useShader();
+        
+    }
 
     return 0;
 }
